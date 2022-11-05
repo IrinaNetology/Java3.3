767040c6c10ca3c1adbb505128565f01b31504a5
Irina <irakali86@gmail.com>
2022-11-05 17:25:45 +0300

    formating

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,12 +1,11 @@
 public class CreditPaymentService {
-    public int calculate (int period , int amount)
-    {
-        double year_percent = 9.99;
-        double month_percent = year_percent / 12 / 100;
+    public int calculate(int period, int amount) {
+        double yearPercent = 9.99;
+        double monthPercent = yearPercent / 12 / 100;
 
-        double a = Math.pow( 1+month_percent , period);
+        double a = Math.pow(1 + monthPercent, period);
 
-        double result = amount * (month_percent + (month_percent / (a - 1)));
+        double result = amount * (monthPercent + (monthPercent / (a - 1)));
 
         return (int) result;
     }
